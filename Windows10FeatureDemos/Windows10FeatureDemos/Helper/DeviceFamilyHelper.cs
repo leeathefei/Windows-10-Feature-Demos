ea28df15c7fcda363644d0cdc5ff31bbf3ca5ced
Oliver Scheer <oliver.scheer@hotmail.de>
2015-09-29 08:56:30 +0200

    DeviceFamilyHelper

---
+++ Windows10FeatureDemos/Windows10FeatureDemos/Helper/DeviceFamilyHelper.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Windows10FeatureDemos.Helper
+{
+    public class DeviceFamilyHelper
+    {
+        private static string _deviceFamily;
+
+        public string DeviceFamily
+        {
+            get
+            {
+                if (_deviceFamily == null)
+                {
+                    _deviceFamily = Windows
+                        .ApplicationModel
+                        .Resources
+                        .Core
+                        .ResourceContext
+                        .GetForCurrentView().QualifierValues["DeviceFamily"];
+                }
+                return _deviceFamily;
+
+                // Results: Desktop, Mobile, ....
+            }
+        }
+
+        public bool IsDesktop
+        {
+            get { return DeviceFamily == "Desktop"; }
+        }
+    }
+}
