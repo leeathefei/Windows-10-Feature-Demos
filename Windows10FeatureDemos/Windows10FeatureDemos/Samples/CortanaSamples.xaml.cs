291c840910c2a186b205892d734ea3f6a2a2ee05
Oliver Scheer <oliver.scheer@hotmail.de>
2015-09-30 12:59:26 +0200

    Updated Start Page,

---
+++ Windows10FeatureDemos/Windows10FeatureDemos/Samples/CortanaSamples.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows10FeatureDemos.Helper;
+
+namespace Windows10FeatureDemos.Samples
+{
+    [Menu(Caption = "Cortana", Symbol = Symbol.Character)]
+    public sealed partial class CortanaSamples : UserControl
+    {
+        // TODO: Cortana Demos
+        public CortanaSamples()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
---
+++ Windows10FeatureDemos/Windows10FeatureDemos/Samples/HubSample.xaml.cs
@@ -0,0 +1,15 @@
+﻿using Windows.UI.Xaml.Controls;
+using Windows10FeatureDemos.Helper;
+
+namespace Windows10FeatureDemos.Samples
+{
+    [Menu(Caption = "Hub Control")]
+    public sealed partial class HubSample : UserControl
+    {
+        // TODO: Hub Sample implementieren
+        public HubSample()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
